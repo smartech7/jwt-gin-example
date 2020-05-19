f34452f11609ed6694703e054bf3c97c57aa0b14
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2020-05-19 17:37:00 +0200

    docs: update comments of SigningAlgorithm field (#246)

--- auth_jwt.go
+++ auth_jwt.go
@@ -25,7 +25,7 @@ type GinJWTMiddleware struct {
 	// Realm name to display to the user. Required.
 	Realm string
 
-	// signing algorithm - possible values are HS256, HS384, HS512
+	// signing algorithm - possible values are HS256, HS384, HS512, RS256, RS384 or RS512
 	// Optional, default is HS256.
 	SigningAlgorithm string
 
